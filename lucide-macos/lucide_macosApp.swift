3546ec30796539b760be117ec605aa0bcdd3fb27
ftgs <182904030+Footagesus@users.noreply.github.com>
2026-03-26 14:25:59 +0500

    Initial Commit

---
+++ lucide-macos/lucide_macosApp.swift
@@ -0,0 +1,17 @@
+//
+//  lucide_macosApp.swift
+//  lucide-macos
+//
+//  Created by oftgs on 26.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct lucide_macosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
